fd57f56c8f1804c12671f0cf95b30e3ac6dd5966
Brian Heithaus <bheithaus@gmail.com>
2012-07-30 21:13:58 -0700

    doin stuff

---
+++ Food Forest/GameView.h
@@ -0,0 +1,13 @@
+//
+//  GameView.h
+//  Food Forest
+//
+//  Created by Brian Heithaus on 7/29/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameView : UIView
+
+@end
---
+++ Food Forest/GameView.m
@@ -0,0 +1,31 @@
+//
+//  GameView.m
+//  Food Forest
+//
+//  Created by Brian Heithaus on 7/29/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "GameView.h"
+
+@implementation GameView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
